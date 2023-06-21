5bddbcc95d8b996b4cb9c7015582c87efdbd9ede
Julia Lepner <ylepner@gmail.com>
2023-06-21 16:27:16 +0200

    feat: add while cycle

--- index.js
+++ index.js
@@ -4,20 +4,32 @@ import { homedir } from 'os';
 const readline = createInterface({
   input: process.stdin,
   output: process.stdout
-});
-
+})
 
 async function run() {
   console.log(`Welcome to the File Manager, ${getUserName(process.argv)}!`)
   console.log(`You are currently in ${getHomeDir()}`)
+  console.log('Please, enter the command')
+  while (true) {
+    const command = await readline.question('')
+    if (command.trim() === '.exit') {
+      readline.question(`Thank you for using File Manager, ${getUserName(process.argv)}, goodbye! \n`)
+      process.exit()
+    }
+    else {
+      console.log('Invalid input. Try another command')
+    }
+  }
 }
 
 await run();
 
 function getUserName(args) {
   const key = '--username=';
-  const userName = args.find(val => val.startsWith(key))?.slice(key.length)
-  if (userName) return userName
+  const user = args.find(val => val.startsWith(key))?.slice(key.length)
+  if (user) {
+    return user
+  }
   return 'No Name'
 }
 
