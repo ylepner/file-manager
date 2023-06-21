2384f3e511a367ba0c44099f7b329bc5e8c9126a
Julia Lepner <ylepner@gmail.com>
2023-06-21 09:33:23 +0200

    feat: get user name and home dir

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('hello!')
+import { createInterface } from 'readline/promises';
+import { homedir } from 'os';
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+async function run() {
+  console.log(`Welcome to the File Manager, ${getUserName(process.argv)}!`)
+  console.log(`You are currently in ${getHomeDir()}`)
+}
+
+await run();
+
+function getUserName(args) {
+  const key = '--username=';
+  const userName = args.find(val => val.startsWith(key))?.slice(key.length)
+  if (userName) return userName
+  return 'No Name'
+}
+
+function getHomeDir() {
+  return homedir();
+}
